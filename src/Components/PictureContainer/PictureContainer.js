3b87da4010d6abcdd190d889c455b8b9a2a505c3
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-08-27 10:45:42 -0500

    Updated To get Info

--- src/Components/PictureContainer/PictureContainer.js
+++ src/Components/PictureContainer/PictureContainer.js
@@ -2,20 +2,20 @@ import React from "react";
 import "./PictureContainer.css";
 
 
-export function Body() {
+export function Body(props) {
     return (
         <div class="bodyContain">
             <div class="picBox">
-                <img id="mainPic" src="https://cdn.mos.cms.futurecdn.net/fXzVgQgE4rFKhzWzMBVHCg.jpg"></img>
+                <img id="mainPic" src={props.pic} alt="Random Planet"/>
             </div>
             <div class="infoBox">
                 <div class="pictureTitle">
-                    <h2>Photo of Earth</h2>
+                    <h2>{props.title}</h2>
                 </div>
                 <hr class="hrWidth">
                 </hr>
                 <p>
-                    Lorem ipsum dolor sit amet, dui nunc ac ut mi morbi porttitor, lacinia velit, et risus eleifend tellus euismod vestibulum, ante ante id bibendum. Diam elit a, felis nullam lacus placerat nulla ultrices, luctus quam mauris sapien tincidunt. Et condimentum risus, dolor molestie odio, vestibulum nam nisl. Vel et non montes amet, eu vestibulum malesuada, sem nullam. Quis a duis varius sed aliquam, odio id condimentum donec, eu nullam fusce hendrerit. Natoque ultricies nunc ante est lobortis dolor, aliquam turpis sem, nibh elit. Consequat vestibulum porta erat vitae, sed scelerisque, aliquet massa non, amet habitasse nullam orci molestie mi. Nulla mollis, libero morbi pede dui vitae faucibus auctor, mattis cursus.
+                    {props.body}
                 </p>
             </div>
         </div>
